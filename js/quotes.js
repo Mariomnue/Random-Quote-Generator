d43ef117621429b032aa981a760e4bd143845cb0
Mariomnue <mary.pienzi@gmail.com>
2020-04-25 14:28:29 -0400

    Added "quotes.js"

---
+++ js/quotes.js
@@ -0,0 +1,89 @@
+/***
+ * `quotes` array
+***/
+var quotes = [
+  { quote: "You know you're in love when you can't fall asleep because reality is finally better than your dreams.",
+    source: "Dr. Seuss",
+    citation: "",
+    year: ""
+  },
+  {
+    quote: "A friend is someone who knows all about you and still loves you.",
+    source: "Elbert Hubbard",
+    citation: "",
+    year: ""
+  },
+  {
+    quote: "We waste time looking for the perfect lover, instead of creating the perfect love.",
+    source: "Tom Robbins",
+    citation: "",
+    year: ""
+  },
+  {
+    quote: "It kills me sometimes, how people die.",
+    source: "Markus Zusak, The Book Thief",
+    citation: "",
+    year: ""
+  },
+  {
+    quote: "Books are a uniquely portable magic.",
+    source: "Stephen King, On Writing: A Memoir of the Craft",
+    citation: "",
+    year: ""
+  },
+  {
+    quote: "′Classic′ - a book which people praise and don't read.",
+    source: "Mark Twain",
+    citation: "",
+    year: ""
+  },
+  {
+    quote: "I like the night. Without the dark, we'd never see the stars.",
+    source: "Stephenie Meyer, Twilight",
+    citation: "",
+    year: ""
+  },
+  {
+    quote: "Be faithful in small things because it is in them that your strength lies.",
+    source: "Mother Teresa",
+    citation: "",
+    year: ""
+  },
+  {
+    quote: "Where there is no hope, it is incumbent on us to invent it.",
+    source: "Albert Camus",
+    citation: "",
+    year: ""
+  },
+  {
+    quote: "Rock bottom became the solid foundation on which I rebuilt my life.",
+    source: "J.K. Rowling",
+    citation: "",
+    year: ""
+  },
+  {
+    quote: "May your choices reflect your hopes, not your fears.",
+    source: "Nelson Mandela",
+    citation: "",
+    year: ""
+  },
+  {
+    quote: "Time is a drug. Too much of it kills you.",
+    source: "Terry Pratchett, Small Gods",
+    citation: "",
+    year: ""
+  },
+  {
+    quote: "The invisible and the non-existent look very much alike.",
+    source: "Delos Banning McKown",
+    citation: "",
+    year: ""
+  },
+  {
+    quote: "Don't waste your time with explanations: people only hear what they want to hear.",
+    source: "Paulo Coelho",
+    citation: "",
+    year: ""
+  },
+
+]
--- js/script.js
+++ js/script.js
@@ -3,14 +3,14 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
+// For assistance:
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
+/***
+ * `quotes` array
 ***/
-
+//see quotes.js
 
 
 /***
